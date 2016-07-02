a165be04828ddbf384c4d20809900a64d1d8b57d
ACharLuk <acharluk@gmail.com>
2016-07-03 01:05:07 +0200

    Started init and dump functions

--- parts/memory.lua
+++ parts/memory.lua
@@ -1,16 +1,41 @@
 memory = {}
 
-function memory:new(name)
+function memory:new(name, file)
  	self = {}
  	setmetatable(self, memory)
 
  	self.name = name
+ 	self.file = file
+ 	self.data = {}
 
  	log(LOG_LEVEL.INFO, "Created memory: name = " .. name)
  	return self
  end
 
+function memory:initialize()
+	local f_handle = io.open(self.file, 'rb')
 
+	-- Load each byte into data
+	for i = 0x0, MEMORY_SIZE do
+		local byte = f_handle:read(1)
+		-- Parse to binary
+		-- self.data[i] = parsed_byte
+	end
+
+
+	f_handle:close()
+end
+
+function memory:dump()
+	local f_handle = io.open(file, 'wb')
+
+	for i = 0x0, MEMORY_SIZE do
+		-- Parse to hex
+		-- f_handle:write( string.char(parsed_byte) )
+	end
+
+	f_handle:close()
+end
 
 function memory.createBlank(file)
 	local f_handle = io.open(file, 'wb')
