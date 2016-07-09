cae0abce929dca8a8fe11d8f8aadd53e597d46ba
ACharLuk <acharluk@gmail.com>
2016-07-09 12:58:36 +0200

    Added some logic functions

--- lib/architecture.lua
+++ lib/architecture.lua
@@ -1,20 +1,70 @@
 architecture = {
 	
 	[0x00] = {
-	name = "HALT",
-	f = function()
-		CPU_RUNNING = false
-		log(LOG_LEVEL.INFO, "CPU stopped")
-	end
+		name = "HALT",
+		f = function()
+			CPU_RUNNING = false
+			log(LOG_LEVEL.INFO, "CPU stopped")
+		end
 	},
 
-	-- ALU Add pc+1 pc+2
 	[0x01] = {
-	name = "ALU ADD $$PC+1 $$PC+2",
-	f = function()
-		n_bytes = 3
-		alu:add(mem:getp(pc + 1), mem:getp(pc + 2))
-	end
+		name = "ALU ADD $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:add(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
 	},
+
+	[0x02] = {
+		name = "ALU SUB $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:sub(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+	[0x03] = {
+		name = "ALU MUL $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:mul(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+	[0x04] = {
+		name = "ALU DIV $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:div(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+	[0x11] = {
+		name = "ALU OR $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:bor(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+	[0x12] = {
+		name = "ALU AND $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:band(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+	[0x13] = {
+		name = "ALU XOR $$PC+1 $$PC+2",
+		f = function()
+			n_bytes = 3
+			alu:bxor(mem:getp(pc + 1), mem:getp(pc + 2))
+		end
+	},
+
+
+
 
 }
--- parts/alu.lua
+++ parts/alu.lua
@@ -18,7 +18,7 @@ end
 function alu:initialize()
 	log(LOG_LEVEL.INFO, self.name .. "->Init")
 	self.alu_register = register:new(self.name .. ">Register ALU")
-	
+
 end
 
 function alu:add(num_1, num_2)
@@ -26,4 +26,46 @@ function alu:add(num_1, num_2)
 	self.alu_register:set(num_1 + num_2)
 
 	log(LOG_LEVEL.INFO, self.name .. "->Add: " .. num_1 .. " + " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:sub(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(num_1 - num_2)
+
+	log(LOG_LEVEL.INFO, self.name .. "->Sub: " .. num_1 .. " - " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:mul(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(num_1 * num_2)
+
+	log(LOG_LEVEL.INFO, self.name .. "->Sub: " .. num_1 .. " * " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:div(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(math.floor(num_1 / num_2))
+
+	log(LOG_LEVEL.INFO, self.name .. "->Div: " .. num_1 .. " / " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:bor(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(bit32.bor(num_1, num_2))
+
+	log(LOG_LEVEL.INFO, self.name .. "->Or: " .. num_1 .. " or " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:band(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(bit32.band(num_1, num_2))
+
+	log(LOG_LEVEL.INFO, self.name .. "->And: " .. num_1 .. " and " .. num_2 .. " = " .. self.alu_register:get())
+end
+
+function alu:bxor(num_1, num_2)
+	-- This is really WIP
+	self.alu_register:set(bit32.bxor(num_1, num_2))
+
+	log(LOG_LEVEL.INFO, self.name .. "->Xor: " .. num_1 .. " xor " .. num_2 .. " = " .. self.alu_register:get())
 end
